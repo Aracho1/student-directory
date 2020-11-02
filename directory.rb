4a571310bd93a13efaaeeb0f13448a7d5922d180
Ara <hello.ara@pm.me>
2020-11-02 20:43:06 +0900

    added a few lines

--- directory.rb
+++ directory.rb
@@ -9,3 +9,8 @@ puts "The Wicked Witch of the West"
 puts "Terminator"
 puts "Freddy Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have "
+# it's important that print() doesn't add new line characters
+print 11
+puts " great students"
