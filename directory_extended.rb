5d8f9c135d6918fc940c8455cee4c776d8a77c1d
Ara <hello.ara@pm.me>
2020-11-10 23:51:42 +0900

    step 8 number 4 edited

--- directory_extended.rb
+++ directory_extended.rb
@@ -41,25 +41,43 @@ end
 # To do this, you'll need to get a list of all existing cohorts
 # (the map() method may be useful but it's not the only option),
 # iterate over it and only print the students from that cohort.
+students =[
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Baratheon", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
 
 def print_by_cohort(students)
   # create an empty array
   students_by_cohort = {}
+  student_number = students.length - 1
 
-  students.each do |student|
-    name = student[:name]
-    cohort = student[:cohort]
+  while student_number >= 0 do
+    name = students[student_number][:name]
+    cohort = students[student_number][:cohort]
 
     if students_by_cohort[cohort] == nil
       students_by_cohort[cohort] = [name]
     else
       students_by_cohort[cohort].push(name)
     end
+
+    student_number -= 1
   end
 
   puts "Enter which cohort to display: "
 
   input = gets.chomp.downcase
 
-  students_by_cohort[input.to_sym]
+  puts students_by_cohort[input.to_sym]
 end
+
+print_by_cohort(students)
